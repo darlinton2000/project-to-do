05636ee02c64b3c8eb3a7e23318ed689a75b6af9
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-01-05 21:24:51 -0300

    Criado as seeders

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Task;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Task::create([
+            'title' => 'Minha Task de Exemplo',
+            'due_date' => '2023-12-12 00:00:00',
+            'description' => 'Texto teste',
+            'user_id' => 1,
+            'category_id' => 1
+        ]);
+    }
+}
