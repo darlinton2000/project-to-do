7050ea152ebc8d83291c5b01d0e512d4bbdd9843
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-01-11 16:33:13 -0300

    Criado as controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +16,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/login', function () {
     return view('login');
-});
+}); */
+
+// Home
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Task
+Route::get('/task/new', [TaskController::class, 'create'])->name('task.create');
+
+// Authentication
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+
+
