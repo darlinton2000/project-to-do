6d6c8e163e54e8c18d932901facb7d0f0e466f59
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-01-17 21:18:55 -0300

    Form registrar-se criado

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -12,5 +13,19 @@ class AuthController extends Controller
 
     public function register(Request $request){
         return view('register');
+    }
+
+    public function register_action(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $data = $request->only('name', 'email', 'password');
+
+        $userCreated = User::create($data);
+
+        return redirect(route('login'));
     }
 }
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,10 +1,38 @@
 <x-layout page="To-Do Login">
 
     <x-slot:btn>
-        <a href="{{route('login')}}" class="btn btn-primary">
+        <a href="{{ route('login') }}" class="btn btn-primary">
             Já possui conta? Faça login
         </a>
     </x-slot:btn>
 
-    Tela de Registro
+    <section id="task_section">
+        <h1> Registrar-se </h1>
+        <form method="POST" action="{{ Route('user.register_action') }}">
+            @if ($errors->any())
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            @endif
+            {{-- flashmsg para erro na tela de login --}}
+            @if (session()->has('loginError'))
+                <div>
+                    <li>{{ session()->get('loginError') }}</li>
+                </div>
+            @endif
+
+            @csrf
+
+            <x-form.text_input name="name" label="Seu nome" placeholder="Seu nome" required="required" />
+            <x-form.text_input type="email" name="email" label="Seu email" placeholder="Seu email"
+                required="required" />
+            <x-form.text_input type="password" name="password" label="Sua senha" placeholder="Sua senha"
+                required="required" />
+            <x-form.text_input type="password" name="password_confirmation" label="Confirme sua Senha"
+                placeholder="Confirme sua Senha" required="required" />
+
+            <x-form.form_button resetTxt="Limpar" submitTxt="Registrar-se" />
+
+        </form>
+    </section>
 </x-layout>
