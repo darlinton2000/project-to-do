1510a3109910cf098edda069549005468944a6f4
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-01-20 20:04:52 -0300

    Protegendo o acesso de algumas rotas com o middleware

--- routes/web.php
+++ routes/web.php
@@ -16,23 +16,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Home
-Route::get('/', [HomeController::class, 'index'])->name('home');
+// Middleware Auth
+Route::middleware(['auth'])->group(function(){
+    // Home
+    Route::get('/', [HomeController::class, 'index'])->middleware('auth')->name('home');
 
-// Task
-Route::get('/task/new', [TaskController::class, 'create'])->name('task.create');
-Route::post('/task/creation_action', [TaskController::class, 'create_action'])->name('task.create_action');
-Route::get('/task/edit', [TaskController::class, 'edit'])->name('task.edit');
-Route::post('/task/edit_action', [TaskController::class, 'edit_action'])->name('task.edit_action');
-Route::get('/task/delete', [TaskController::class, 'delete'])->name('task.delete');
-Route::get('/task', [TaskController::class, 'index'])->name('task.view');
+    // Task
+    Route::get('/task/new', [TaskController::class, 'create'])->name('task.create');
+    Route::post('/task/creation_action', [TaskController::class, 'create_action'])->name('task.create_action');
+    Route::get('/task/edit', [TaskController::class, 'edit'])->name('task.edit');
+    Route::post('/task/edit_action', [TaskController::class, 'edit_action'])->name('task.edit_action');
+    Route::get('/task/delete', [TaskController::class, 'delete'])->name('task.delete');
+    Route::get('/task', [TaskController::class, 'index'])->name('task.view');
+
+    // Logout
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
 // Authentication
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'login_action'])->name('user.login_action');
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'register_action'])->name('user.register_action');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
 
 
 
